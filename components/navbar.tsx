00caaf8d0b3c07cb53a8aaf52b6d075374ef7864
Karmen Dykstra <kldykstra@gmail.com>
2022-07-22 14:32:44 +0200

    Moved blog post date directly below post title

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,11 +4,10 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <header>
-      <div className="absolute top-[4rem] font-lobster text-6xl">
-        Karmen.Page
-      </div>
       <div className="grid grid-cols-6 font-barlow h-12 -mt-6">
-        <div className="col-span-4"></div>
+        <div className="col-span-4 top-[5rem] font-lobster text-3xl md:text-4xl lg:text-5xl">
+          Karmen's Data Science Blog
+        </div>
         <div className="col-span-1 grid place-items-center">
           <Link href="/">
             <button className="bg-hero-color hover:bg-hero-color hover:bg-opacity-50 h-full w-full">Home</button>
--- components/post-header.tsx
+++ components/post-header.tsx
@@ -14,17 +14,15 @@ const PostHeader = ({ title, coverImage, date, author }: Props) => {
   return (
     <>
       <PostTitle>{title}</PostTitle>
+      <div className="mx-auto">
+        <div className="mb-6 text-lg">
+          <DateFormatter dateString={date} />
+        </div>
+      </div>
       <div className="hidden md:block md:mb-12">
       </div>
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} src={coverImage} />
-      </div>
-      <div className="max-w-2xl mx-auto">
-        <div className="block md:hidden mb-6">
-        </div>
-        <div className="mb-6 text-lg">
-          <DateFormatter dateString={date} />
-        </div>
       </div>
     </>
   )
